491f5b50d2c4e24aef2a84f4bbd3ff45fbf52da8
Yash1thvs <yashvanthvs.learning@gmail.com>
2024-02-17 20:43:22 +0530

    - Learning Models - Created User model - Created UserProfile model - Ran migration command

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/index", views.index, name="users_main_view")
+    path("index", views.index, name="users_main_view")
 ]
